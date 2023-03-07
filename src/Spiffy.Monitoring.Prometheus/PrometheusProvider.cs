5b7f11b003fadba77314588e585726eca882cf3f
Chris Peterson <chris.peterson@gettyimages.com>
2023-03-06 16:59:48 -0800

    Add a lock around manipulations to underlying prometheus collections class
    A Prometheus.CollectorRegistry is used by the StartCollection method and it is not thread-safe

--- src/Spiffy.Monitoring.Prometheus/PrometheusProvider.cs
+++ src/Spiffy.Monitoring.Prometheus/PrometheusProvider.cs
@@ -6,6 +6,7 @@ namespace Spiffy.Monitoring.Prometheus
     public static class PrometheusProvider
     {
         static IDisposable _previousCollector = null;
+        static readonly object _serializeCollectionAccess = new object();
 
         public static InitializationApi.ProvidersApi Prometheus(this InitializationApi.ProvidersApi api, Action<PrometheusConfigurationApi> customize = null)
         {
@@ -13,9 +14,12 @@ namespace Spiffy.Monitoring.Prometheus
             {
                 var config = new PrometheusConfigurationApi();
                 customize(config);
-                _previousCollector?.Dispose();
 #if NET6_0_OR_GREATER
-                _previousCollector = config.RuntimeStats.StartCollecting();
+                lock (_serializeCollectionAccess)
+                {
+                    _previousCollector?.Dispose();
+                    _previousCollector = config.RuntimeStats.StartCollecting();
+                }
 #endif
             }
             api.Add("prometheus", PrometheusRules.Process);
